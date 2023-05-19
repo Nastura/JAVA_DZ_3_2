90d2808ea18d073a7e4c9a9b612f4cd1dfb178e1
Nastura <chulmost@gmail.com>
2023-05-19 23:49:56 +0300

    update file

--- src/Main.java
+++ src/Main.java
@@ -9,23 +9,21 @@ public class Main {
         //
 
         int balans = 1400;
-        int add_rub = 1125;
+        int addRub = 1125;
 
         int total;
         int present;
 
-        if(add_rub > 1000) {
-            present = add_rub / 100;
-            total = balans + add_rub + present;
+        if (addRub > 1000) {
+            present = addRub / 100;
+            total = balans + addRub + present;
         } else {
             present = 0;
-            total = balans + add_rub;
+            total = balans + addRub;
         }
 
-
-
         System.out.println("Текущий баланс: " + balans);
-        System.out.println("Сумма пополнения: " + add_rub);
+        System.out.println("Сумма пополнения: " + addRub);
         System.out.println("Подарочные минуты: " + present);
         System.out.println("Остаток: " + total);
 
